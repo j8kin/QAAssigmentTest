10df1d141ad8730f3480e4893ad6d5a9c9f941bc
Eugene <37618680+j8kin@users.noreply.github.com>
2024-11-23 20:12:39 +0100

    Add Plugin Version Plugin Add Display All Gutters Icons Plugin

--- src/main/kotlin/org/qatest/plugin/demo/PluginVersionAction.kt
+++ src/main/kotlin/org/qatest/plugin/demo/PluginVersionAction.kt
@@ -1,19 +1,15 @@
 package org.qatest.plugin.demo
 
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.ui.Messages
 import org.jetbrains.annotations.NotNull
 
-class PopupDialogAction : AnAction() {
-//    override fun update(@NotNull event: AnActionEvent) {
-//        // Using the event, evaluate the context,
-//        // and enable or disable the action.
-//    }
-
+class PluginVersionAction : AnAction() {
     override fun actionPerformed(@NotNull event: AnActionEvent) {
-        // Using the event, implement an action.
-        // For example, create and show a dialog.
-        Messages.showMessageDialog("QATestPlugin Message", "QATestPlugin", Messages.getInformationIcon())
-    } // Override getActionUpdateThread() when you target 2022.3 or later!
+        val pluginVer = PluginManagerCore.getPlugin(PluginId.getId("org.qatest.plugin.demo"))?.version
+        Messages.showMessageDialog("version: $pluginVer", "QATestPlugin version", Messages.getInformationIcon())
+    }
 }
